74471e555cd7725141fe240e61e2158a17fba4fc
wangxin <vino0908@outlook.com>
2023-08-11 15:22:46 +0800

    配置vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,36 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Unocss from "unocss/vite"
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /\/@\//,
-        replacement: fileURLToPath(new URL('./src', import.meta.url)) + '/'
-      },
-      {
-        find: /\/#\//,
-        replacement: fileURLToPath(new URL('./types', import.meta.url)) + '/'
+export default defineConfig((configEnv:ConfigEnv) => {
+  const env = loadEnv(configEnv.mode, process.cwd())
+  return {
+    server: {
+      port: 8000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(new RegExp(`^/api`), ''),
+        }
       }
-    ],
-  },
-  plugins: [Unocss(), react()],
-})
+    },
+    resolve: {
+      alias: [
+        {
+          find: /\/@\//,
+          replacement: fileURLToPath(new URL('./src', import.meta.url)) + '/'
+        },
+        {
+          find: /\/#\//,
+          replacement: fileURLToPath(new URL('./types', import.meta.url)) + '/'
+        }
+      ],
+    },
+    plugins: [Unocss(), react()],
+  }
+}
+)
