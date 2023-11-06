30063d7585f42bd570d79f98fdbb6d8c24c3c27f
vino <vino0908@outlook.com>
2023-11-06 23:26:38 +0800

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import {Suspense,lazy} from 'react'
+import { createRoot } from 'react-dom/client'
 import './global.css'
-import { App } from './App'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Login = lazy(() => import('./views/Login').then(r => ({default: r.Login})))
+
+const container = document.getElementById('root')
+
+
+const App = () => (
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
 )
+
+container &&  createRoot(container).render(<App />)
